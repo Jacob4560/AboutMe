02e5d5eb4a00fc445ca59856c549f9c6b6998194
Jacob Lush <44276721+Jacob4560@users.noreply.github.com>
2023-01-02 21:22:42 -0800

    Finished content for Story page, updated navbar

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 This repository contains Javscript, HTML, and CSS files for my portfolio website.
-Uses icons from Font Awesome. 
-Pictures used are either taken by me or are stock images from Unsplash.
+Uses icons from Font Awesome.
--- script.js
+++ script.js
@@ -6,7 +6,19 @@ window.addEventListener("load", function () {
         .forEach((item, index) => {
             item.onmouseover = () => {
                 menu.setAttribute("activeIndex", index);
-                console.log(menu.activeIndex);
+            }
+        });
+        
+    const container = document.getElementById("container");
+    
+    Array.from(document.getElementsByClassName("storyimg"))
+        .forEach((item, index) => {
+            item.onmouseover = () => {
+                container.setAttribute("shrink", 1);
+                container.setAttribute("activeIndex", index);
+            }
+            item.onmouseout = () => {
+                container.setAttribute("shrink", 0);
             }
         });
     
